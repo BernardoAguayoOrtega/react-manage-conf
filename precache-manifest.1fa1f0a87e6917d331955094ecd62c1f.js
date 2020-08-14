ee3bb7c97dceca635de7e6164b487ccb114847bb
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-14 12:55:28 -0500

    Updates

---
+++ precache-manifest.1fa1f0a87e6917d331955094ecd62c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226635702889cfff92e62ad23c07676e",
+    "url": "/react-manage-conf/badges/index.html"
+  },
+  {
+    "revision": "a23ebfde84914b5120b0",
+    "url": "/react-manage-conf/badges/static/css/main.2eae7b07.chunk.css"
+  },
+  {
+    "revision": "365daeb2eeebf4e5c431",
+    "url": "/react-manage-conf/badges/static/js/2.8229fc91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-manage-conf/badges/static/js/2.8229fc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23ebfde84914b5120b0",
+    "url": "/react-manage-conf/badges/static/js/main.244b1b5d.chunk.js"
+  },
+  {
+    "revision": "5c9ab1064d6e8763ab75",
+    "url": "/react-manage-conf/badges/static/js/runtime-main.c0d5c353.js"
+  }
+]);
